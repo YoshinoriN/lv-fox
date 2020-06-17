ec6b702dd0118d7e3970e5150416886fb5241189
YoshinoriN <nayo-msoft@outlook.jp>
2020-06-17 23:27:52 +0900

    refactor: move dbmigration from components to loader and config class to object

--- src-backend/app/components/LvFoxLoader.scala
+++ src-backend/app/components/LvFoxLoader.scala
@@ -1,13 +1,17 @@
 package components
 
+import config.Config
+import modules.DbMigration
 import play.api.{Application, ApplicationLoader}
 import play.api.ApplicationLoader.Context
 
 class LvFoxLoader extends ApplicationLoader {
 
+  private val dbMigration = new DbMigration(Config.dbUrl, Config.dbUser, Config.dbPassword)
+
   def load(context: Context): Application = {
+    dbMigration.migrate()
     val lvFoxComponents: LvFoxComponents = new LvFoxComponents(context)
-    lvFoxComponents.dbMigration.migrate()
     lvFoxComponents.application
   }
 
