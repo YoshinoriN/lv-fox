c5beeed6bc68ad36b81d3f4c2ce1db615fc90cbd
YoshinoriN <nayo-msoft@outlook.jp>
2020-06-25 06:51:03 +0900

    chore: add TODOs

--- src-backend/app/controllers/PageController.scala
+++ src-backend/app/controllers/PageController.scala
@@ -6,10 +6,12 @@ import services.PagesService
 
 class PageController @Inject()(controllerComponents: ControllerComponents, pagesService: PagesService) extends AbstractController(controllerComponents) {
 
+  // TODO: CORS, Auth(with Action)
   def search(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok("{\"status\":\"todo - get\"}").as(JSON)
   }
 
+  // TODO: Auth(with Action), IpFilter(With Action), replace ignore Chars(with Action)
   def upsert: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok("{\"status\":\"todo - post\"}").as(JSON)
   }
