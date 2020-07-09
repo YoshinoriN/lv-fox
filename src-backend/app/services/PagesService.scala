6f02b54f6e42a1b23a87db9e3cdaf5b1fc7c5baa
YoshinoriN <nayo-msoft@outlook.jp>
2020-07-09 23:40:41 +0900

    refactor: change argument name

--- src-backend/app/services/PagesService.scala
+++ src-backend/app/services/PagesService.scala
@@ -29,19 +29,19 @@ class PagesService(pagesRepository: PagesRepository) {
     words.flatMap(_.r.findAllMatchIn(sentence).map(s => (s.start - 8, s.end + 8)).toSeq).sortBy(_._1)
 
   @tailrec
-  private def createNewSeq(indexes: Seq[(Int, Int)], acc: Seq[(Int, Int)] = Nil): Seq[(Int, Int)] = {
-    if (indexes.nonEmpty) {
-      val i = indexes.head
-      indexes.lift(1) match {
+  private def createNewSeq(idxes: Seq[(Int, Int)], acc: Seq[(Int, Int)] = Nil): Seq[(Int, Int)] = {
+    if (idxes.nonEmpty) {
+      val i = idxes.head
+      idxes.lift(1) match {
         case None =>
-          if (acc == Nil) createNewSeq(indexes.tail, Seq(indexes.head))
+          if (acc == Nil) createNewSeq(idxes.tail, Seq(idxes.head))
           else acc
         case Some(n) =>
           if (n._1 > i._2) {
-            createNewSeq(indexes.tail, Seq(i, n))
+            createNewSeq(idxes.tail, Seq(i, n))
           } else {
-            if (acc.size > 1 && acc.tail.head._1 == i._1) createNewSeq(indexes.tail, acc.dropRight(1) :+ (i._1, n._2))
-            else createNewSeq(indexes.tail, acc :+ (i._1, n._2))
+            if (acc.size > 1 && acc.tail.head._1 == i._1) createNewSeq(idxes.tail, acc.dropRight(1) :+ (i._1, n._2))
+            else createNewSeq(idxes.tail, acc :+ (i._1, n._2))
           }
       }
     } else {
@@ -50,12 +50,12 @@ class PagesService(pagesRepository: PagesRepository) {
   }
 
   @tailrec
-  private def substrRecursively(sentence: String, indexes: Seq[(Int, Int)], current: Int = 0, acc: String = ""): String = {
-    if (indexes.size > current) {
-      val i = indexes(current)
+  private def substrRecursively(sentence: String, idxes: Seq[(Int, Int)], current: Int = 0, acc: String = ""): String = {
+    if (idxes.size > current) {
+      val i = idxes(current)
       substrRecursively(
         sentence,
-        indexes,
+        idxes,
         current + 1,
         acc + sentence.slice(i._1, i._2) + "...    "
       )
