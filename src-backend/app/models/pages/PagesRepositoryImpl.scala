b2fed4463e67a95e671fda9434acaa8dc9af2de6
YoshinoriN <nayo-msoft@outlook.jp>
2020-07-11 16:02:29 +0900

    feat: add limit to search result

--- src-backend/app/models/pages/PagesRepositoryImpl.scala
+++ src-backend/app/models/pages/PagesRepositoryImpl.scala
@@ -14,7 +14,7 @@ class PagesRepositoryImpl extends PagesRepository {
       0,
       query[Pages].filter(p => p.content like lift(s"%${words.head}%"))
     )
-    ctx.run(q)
+    ctx.run(q.take(30))
   }
 
   override def upsert(pages: Pages): Pages = {
