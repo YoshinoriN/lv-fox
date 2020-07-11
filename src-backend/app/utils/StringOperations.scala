9f120ac40229b2de51d699bad2314e5b063b156c
YoshinoriN <nayo-msoft@outlook.jp>
2020-07-11 18:18:38 +0900

    feat: add filter ignore chars method

--- src-backend/app/utils/StringOperations.scala
+++ src-backend/app/utils/StringOperations.scala
@@ -1,7 +1,19 @@
 package utils
 
 object StringOps {
+
+  private val ignoreCharsRegex = "[!\"#$%&'()-^\\@[;:],./\\=~|`{+*}<>?_、。，．・：；？！゛゜´｀¨＾￣＿]"
+
   implicit class StringOps(val s: String) {
+
     def stripHtmlTags: String = s.replaceAll("""<(\"[^\"]*\"|'[^']*'|[^'\">])*>""", "")
+
+    def hasIgnoreChars: Boolean = ignoreCharsRegex.r.findFirstMatchIn(s) match {
+      case None => false
+      case Some(_) => true
+    }
+
+    def filterIgnoreChars: String = s.replaceAll(ignoreCharsRegex, "")
+
   }
 }
--- src-backend/test/utils/StringOpsSpec.scala
+++ src-backend/test/utils/StringOpsSpec.scala
@@ -16,4 +16,44 @@ class StringOpsSpec extends PlaySpec {
 
   }
 
+  "StringOps - hasIgnoreChars" should {
+
+    "detected ignore chars" in {
+      "test;test".hasIgnoreChars mustBe true
+      "\\testtest".hasIgnoreChars mustBe true
+      "%testtest".hasIgnoreChars mustBe true
+      "testtest%".hasIgnoreChars mustBe true
+      "test,test".hasIgnoreChars mustBe true
+      "testtest*".hasIgnoreChars mustBe true
+      "testtes*t".hasIgnoreChars mustBe true
+      "test&test".hasIgnoreChars mustBe true
+      "testt@est".hasIgnoreChars mustBe true
+      "te/sttest".hasIgnoreChars mustBe true
+      "tes.ttest".hasIgnoreChars mustBe true
+      "test。test".hasIgnoreChars mustBe true
+      "test、test".hasIgnoreChars mustBe true
+      "test。test".hasIgnoreChars mustBe true
+      "testte、st".hasIgnoreChars mustBe true
+      "testtes＿t".hasIgnoreChars mustBe true
+      "testtes_t".hasIgnoreChars mustBe true
+      "testtes(t".hasIgnoreChars mustBe true
+      "testtes)t".hasIgnoreChars mustBe true
+      "testtesあt".hasIgnoreChars mustBe false
+      "testtesいt".hasIgnoreChars mustBe false
+      "testtest".hasIgnoreChars mustBe false
+    }
+
+  }
+
+  "StringOps - filterIgnoreChars" should {
+
+    "filterd ignore chars" in {
+      "testtesあt".filterIgnoreChars mustBe "testtesあt"
+      "testtesいt".filterIgnoreChars mustBe "testtesいt"
+      "test$%^&@!test".filterIgnoreChars mustBe "testtest"
+      "\\acb。.def///ghi%_jkl*mn**op$qr{}s\\tuあいう。。えお、かきく!!け!こ".filterIgnoreChars mustBe "acbdefghijklmnopqrstuあいうえおかきくけこ"
+    }
+
+  }
+
 }
