631856f545c107fbd97fa2b906c7387e1bc32e88
YoshinoriN <nayo-msoft@outlook.jp>
2020-07-19 21:33:45 +0900

    fix: get IP address

--- src-backend/app/utils/Network.scala
+++ src-backend/app/utils/Network.scala
@@ -5,8 +5,8 @@ import play.api.mvc.Request
 trait Network {
 
   def getIpAddress[A](request: Request[A]): String = {
-    val xForwardedFor = request.headers.get("X-Forwarded-For").getOrElse("")
-    xForwardedFor.split(",").headOption.getOrElse(request.remoteAddress)
+    val xForwardedFor = request.headers.get("X-Forwarded-For").getOrElse(request.remoteAddress)
+    xForwardedFor.split(",").head
   }
 
 }
